88cab8bbe8401dd1679cba243581b1acfdadb91b
T_Rexan <tamara1819@yahoo.com>
2023-03-10 10:19:03 +0100

    added search function

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import LaunchItem from '@/components/Launches/launch-item';
 import formatDate from '@/utils/formatDate';
 
+import CancelIcon from '../public/icons/close-icon.svg';
 import styles from './home.module.scss';
 
 type LaunchItemProps = {
@@ -17,7 +18,9 @@ type LaunchItemProps = {
 
 const Home: NextPage = () => {
   const [launches, setLaunches] = useState([]);
+  const [filteredLaunches, setFilteredLaunches] = useState([]);
   const [hasMore, setHasMore] = useState(true);
+  const [isSearch, setIsSearch] = useState(true);
   const limit = 20;
   const initialOffset = 0;
 
@@ -47,34 +50,69 @@ const Home: NextPage = () => {
     fetchLaunches(initialOffset);
   }, []);
 
+  const searchHandler = async (e) => {
+    setIsSearch(true);
+    try {
+      const response = await fetch('https://api.spacexdata.com/v3/launches');
+      const data = await response.json();
+      const stringQuery = e.target.value;
+      const filteredLaunchesArray = data.filter((launch) =>
+        launch.mission_name.toLowerCase().includes(stringQuery.toLowerCase()),
+      );
+      setFilteredLaunches(filteredLaunchesArray);
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const cancelHandler = () => {};
+
   return (
-    <InfiniteScroll
-      dataLength={launches.length}
-      next={loadMore}
-      hasMore={hasMore}
-      loader={<h4>Loading...</h4>}
-    >
-      <div className={styles.grid}>
-        {launches?.map(
-          ({
-            flight_number,
-            mission_name,
-            links,
-            details,
-            launch_date_local,
-          }: LaunchItemProps) => (
-            <LaunchItem
-              key={flight_number}
-              name={mission_name}
-              id={flight_number}
-              img={links.mission_patch || '/no-image.png'}
-              details={details}
-              date={formatDate(launch_date_local)}
-            />
-          ),
+    <>
+      <div className={styles.search}>
+        <input
+          onSubmit={searchHandler}
+          className={styles.input}
+          type="text"
+          placeholder="Search..."
+        />
+        {isSearch && (
+          <button onClick={cancelHandler} className={styles.cancelBttn}>
+            <span className={styles.icon}>
+              <CancelIcon />
+            </span>
+          </button>
         )}
       </div>
-    </InfiniteScroll>
+      <InfiniteScroll
+        dataLength={launches.length}
+        next={loadMore}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className={styles.grid}>
+          {launches?.map(
+            ({
+              flight_number,
+              mission_name,
+              links,
+              details,
+              launch_date_local,
+            }: LaunchItemProps) => (
+              <LaunchItem
+                key={flight_number}
+                name={mission_name}
+                id={flight_number}
+                img={links.mission_patch || '/no-image.png'}
+                details={details}
+                date={formatDate(launch_date_local)}
+              />
+            ),
+          )}
+        </div>
+      </InfiniteScroll>
+    </>
   );
 };
 
