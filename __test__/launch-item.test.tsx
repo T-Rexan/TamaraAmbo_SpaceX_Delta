18a2bdb73f1800c037ab8be337b8651ca379f424
T_Rexan <tamara1819@yahoo.com>
2023-03-10 14:20:45 +0100

    inital test setup

--- __test__/launch-item.test.tsx
+++ __test__/launch-item.test.tsx
@@ -3,20 +3,49 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 
 import LaunchItem from '../components/Launches/launch-item';
+import styles from '../components/Launches/launch-item/launch-item.module.scss';
 
-describe('LaunchItem', () => {
-  it('renders with the correct props', async () => {
-    const props = {
-      id: '1',
-      name: 'Falcon 9',
-      img: 'https://www.example.com/falcon9.jpg',
-      details: 'Details about Falcon 9 launch',
-      date: '2023-03-10',
-    };
-    const { getByText, getByAltText } = render(<LaunchItem {...props} />);
+describe('LaunchItem component', () => {
+  const props = {
+    id: '1',
+    name: 'Falcon 9',
+    img: 'https://www.example.com/falcon9.jpg',
+    details: 'Details about Falcon 9 launch',
+    date: '2023-03-10',
+  };
+
+  it('should render with the correct props', () => {
+    const { getByAltText, getByText } = render(<LaunchItem {...props} />);
+    expect(getByAltText(props.name)).toBeInTheDocument();
     expect(getByText(props.name)).toBeInTheDocument();
     expect(getByText(props.date)).toBeInTheDocument();
     expect(getByText(props.details)).toBeInTheDocument();
-    expect(getByAltText(props.name)).toBeInTheDocument();
+    expect(getByText('Learn more')).toHaveAttribute('href', `/${props.id}`);
+  });
+
+  it('should render the Image component with correct props', () => {
+    const { getByAltText } = render(<LaunchItem {...props} />);
+    const image = getByAltText(props.name);
+    expect(image).toHaveAttribute('src', props.img);
+    expect(image).toHaveAttribute('width', '500');
+    expect(image).toHaveAttribute('height', '500');
+    expect(image).toHaveAttribute('layout', 'responsive');
+    expect(image).toHaveAttribute('loading', 'eager');
+    expect(image).toHaveAttribute('importance', 'high');
+  });
+
+  it('should render with a Card component', () => {
+    const { container } = render(<LaunchItem {...props} />);
+    expect(container.firstChild).toHaveClass('card');
+  });
+
+  it('should render with the correct class names', () => {
+    const { container } = render(<LaunchItem {...props} />);
+    const grid = container.firstChild?.firstChild;
+    expect(grid).toHaveClass(styles.grid);
+    expect(grid?.firstChild).toHaveClass(styles.title);
+    expect(grid?.childNodes[1]).toHaveClass(styles.date);
+    expect(grid?.childNodes[2]).toHaveClass(styles.details);
+    expect(grid?.lastChild).toHaveClass(styles.link);
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,20 @@
 const nextJest = require('next/jest');
+
 const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 });
+
+// Add any custom config to be passed to Jest
 const customJestConfig = {
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    // Handle module aliases (this will be automatically configured for you soon)
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
+  testEnvironment: 'jest-environment-jsdom',
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
